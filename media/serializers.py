90a8ca3a5eb2d023a64ac837d56efa22a7055a25
iamkorniichuk <iamkorniichuk@gmail.com>
2023-07-27 22:49:47 +0300

    Create Media nested serializer

--- media/serializers.py
+++ media/serializers.py
@@ -3,6 +3,20 @@ from rest_framework import serializers
 from .models import Media
 
 
+class NestedMediaSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        instance = Media.objects.get(**validated_data)
+        return instance
+
+    class Meta:
+        model = Media
+        fields = ("attachment", "attachment_type")
+        extra_kwargs = {
+            "attachment": {"read_only": True},
+            "attachment_type": {"read_only": True},
+        }
+
+
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Media
