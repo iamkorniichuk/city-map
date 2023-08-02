c0f54a856465f718256526c0ec62ed4d0cbc30ae
iamkorniichuk <iamkorniichuk@gmail.com>
2023-08-02 09:28:02 +0300

    Add CRUD for current user

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("logout_all/", LogoutAllView.as_view(), name="logout_all"),
     path("sign_up/", SignUpView.as_view(), name="sign_up"),
+    path("my/", MyUserDetailView.as_view(), name="my_detail"),
 ]
--- users/views.py
+++ users/views.py
@@ -39,3 +39,12 @@ class SignUpView(LoginView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return serializer.create(serializer.validated_data)
+
+
+class MyUserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
